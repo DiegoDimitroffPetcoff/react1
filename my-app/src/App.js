7ddaa2464ab1d703f981434c34bd9b0702fae80b
DiegoDimitroffPetcof <126318451+DiegoDimitroffPetcof@users.noreply.github.com>
2023-02-25 16:44:30 +0000

    Add some Css and NavBar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,8 +11,18 @@ function App() {
 
   return (
     <div>
+      <header>
+        <a href="*" class="logo"><h2>Dimi Games</h2><h6>Never stop</h6></a>
 
-<ExcuseGame></ExcuseGame>
+        <nav>
+          <a href=''>Home</a>
+          <a href='https://se.linkedin.com/in/diego-leonardo-vallejos-dimitroff-petcoff-687b0b1b8/en?original_referer=https%3A%2F%2Fwww.google.com%2F'>About Me</a>
+          <a href=''>Contact us!</a>
+        </nav>
+      </header>
+
+
+      <ExcuseGame></ExcuseGame>
 
     </div>
   );
