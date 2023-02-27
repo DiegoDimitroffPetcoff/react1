38d734aeb018c365068ec77abb33394cbbfbc41e
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2023-02-27 20:04:18 +0000

    Add files & .editorConfig

---
+++ my-app/src/components/pure/header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export function Header() {
+  return (
+    <div>
+      {" "}
+      <header>
+        <a href="*" class="logo">
+          <h2>Dimi Games</h2>
+
+          <h6>Never stop</h6>
+        </a>
+
+        <nav>
+          <a href="">Home</a>
+          <a href="https://se.linkedin.com/in/diego-leonardo-vallejos-dimitroff-petcoff-687b0b1b8/en?original_referer=https%3A%2F%2Fwww.google.com%2F">
+            About Me
+          </a>
+          <a href="">Contact us!</a>
+        </nav>
+      </header>
+    </div>
+  );
+}
