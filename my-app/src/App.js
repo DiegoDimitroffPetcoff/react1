485a37ba774702c9903e23190c9ba4e7874c48b3
DiegoDimitroffPetcof <126318451+DiegoDimitroffPetcof@users.noreply.github.com>
2023-02-28 01:31:05 +0000

    Fix Screen

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import { MenuGame } from "./components/menuGames";
 import { Header } from "./components/pure/header";
 import { Footer } from "./components/pure/footer";
 
+
 function App() {
   return (
     <div>
