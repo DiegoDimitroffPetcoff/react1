97002c2ddae351174a65bff6cecf4efdbe76b7a6
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2023-04-01 13:40:38 +0100

    Add sessionStorage

--- my-app/src/components/auth/Login success.jsx
+++ my-app/src/components/auth/Login success.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-export function LoginSuccess(data) {
-  console.log("data");
-  console.log(data);
+export function LoginSuccess() {
+  //recibo del Session Storage la info de user
+  let user = sessionStorage.getItem("user");
+  let userp = JSON.parse(user);
+
   return (
     <div>
-
       <h1>El logeo ha sido exitoso</h1>
+      Bienvenido {userp.data.firstName}!
     </div>
   );
 }
--- my-app/src/components/pure/form.jsx
+++ my-app/src/components/pure/form.jsx
@@ -5,7 +5,8 @@ import { Formik, Field, Form } from "formik";
 
 export function Login() {
   const Navigate = useNavigate();
-
+  const [logged, setLogged] = useState(false);
+  const [user, setUser] = useState();
   const initialValues = { name: "", password: "" };
 
   return (
@@ -23,27 +24,25 @@ export function Login() {
               if (response.data === "Username Or Password incorrect") {
                 Navigate("/loginfail");
               } else {
-                console.log(response.data);
-                Navigate("loginsuccess", data);
+
+                //guardo en el session storage la info del usuario
+                sessionStorage.setItem("logged", true);
+                sessionStorage.setItem("user", JSON.stringify(response));
+                Navigate("loginsuccess", response);
               }
             });
         }}
       >
         {({ isSubmitting }) => (
           <Form>
-            
-
             <Field id="name" name="name" placeholder="Your Username" />
 
-            
             <Field
               id="pasword"
               type="password"
               name="password"
               placeholder="Pasword"
             />
-
-            
 
             <button type="submit">Login</button>
             {isSubmitting ? <p>Loading</p> : null}
